f1596bcf303c46f8bb79193b2b68a88d5c12b1e0
Baraa Abuhalima <B.K.1Baraa@gmail.com>
2025-07-29 13:30:24 +0300

    Override the ToString method in ProductInventory

--- ProductInventory.cs
+++ ProductInventory.cs
@@ -2,4 +2,15 @@ class ProductInventory : Inventory<string, Product>
 {
     private static readonly ProductInventory instance = new();
     public static ProductInventory Instance => instance;
+    public override string ToString()
+    {
+        var productsList = this.All();
+        string s = "";
+        for (int i = 0; i < productsList.Count; i++)
+        {
+            s += $"{i+1}.  {productsList[i]}\n";
+        }
+        return s;
+    }
+
 }
